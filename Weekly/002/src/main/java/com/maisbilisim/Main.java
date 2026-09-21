8f0e9b62f3c086399869809172a1a0dc7de4a5cb
Arda Seremet <ardaseremet@outlook.com>
2026-09-21 22:33:37 +0200

    Week 2 implementation.

---
+++ Weekly/002/src/main/java/com/maisbilisim/Main.java
@@ -0,0 +1,28 @@
+package com.maisbilisim;
+
+import java.util.Map;
+
+public class Main {
+    private static final Map<String, String> translationMap = Map.ofEntries(
+            Map.entry("hun", "Szia"),
+            Map.entry("eng", "Hello"),
+            Map.entry("tur", "Merhaba"));
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a name and/or a language code as command line arguments.");
+            return;
+        }
+
+        String chosenLanguage = args.length > 1 ? args[1] : "hun";
+        String greeting = translationMap.get(chosenLanguage);
+        if (greeting == null) {
+            System.out.println("Invalid language code. Please use " + translationMap.keySet() + ".");
+            return;
+        }
+
+        String name = args[0];
+
+        System.out.println(greeting + ", " + name + "!");
+    }
+}
