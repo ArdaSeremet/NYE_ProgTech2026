b8119982a5b3b7237f018a6b82f5ea187114e00c
Arda Seremet <ardaseremet@outlook.com>
2026-09-21 22:35:32 +0200

    02: Spanish support.

--- Weekly/002/src/main/java/com/maisbilisim/Main.java
+++ Weekly/002/src/main/java/com/maisbilisim/Main.java
@@ -6,7 +6,8 @@ public class Main {
     private static final Map<String, String> translationMap = Map.ofEntries(
             Map.entry("hun", "Szia"),
             Map.entry("eng", "Hello"),
-            Map.entry("tur", "Merhaba"));
+            Map.entry("tur", "Merhaba"),
+            Map.entry("esp", "Hola"));
 
     public static void main(String[] args) {
         if (args.length == 0) {
